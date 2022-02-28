4515e248d29626073163272a8c6f4394f4637ac0
kensukeohguchi1001 <kensuke.ohguchi@gmail.com>
2022-03-01 06:45:10 +0900

    修正を追加

--- sample.php
+++ sample.php
@@ -1,16 +1,23 @@
 <?php
 // 無名関数を書いてみよう
 
+// $times = 3;
+
+// $numbers = [1,2,3];
+// $products = array_map(fn ($num) => $num * $times,$numbers);
+// var_dump($products);
+
+// function convertToNumber(string ...$cards): array
+// {
+//   return array_map(fn ($card) => substr($card, 1), $cards);
+// }
+
+// $test = convertToNumber('H3', 'S10', 'DA');
+// var_dump($test);
+
+$numbers = [2,4,6];
 $times = 3;
 
-$numbers = [1,2,3];
-$products = array_map(fn ($num) => $num * $times,$numbers);
-var_dump($products);
+$test = array_map(fn ($num) => $num * $times, $numbers);
 
-function convertToNumber(string ...$cards): array
-{
-  return array_map(fn ($card) => substr($card, 1), $cards);
-}
-
-$test = convertToNumber('H3', 'S10', 'DA');
 var_dump($test);
