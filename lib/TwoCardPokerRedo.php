0e5f8a75928014d0988fd045237afe4781a27744
kensukeohguchi1001 <kensuke.ohguchi@gmail.com>
2022-03-03 06:13:59 +0900

    修正を追加

--- lib/TwoCardPokerRedo.php
+++ lib/TwoCardPokerRedo.php
@@ -20,9 +20,6 @@ function showDown($card11, $card12, $car21, $card22): array
     $hands = array_map(fn ($playerCardRank) => checkHand($playerCardRank[0], $playerCardRank[1]), $playerCardRanks);
     return [];
 }
-    // ConvertToNumber関数を作成する('ck'から'k'にする)
-    // 役の判定をする関数を作成(straight,pair,high card)、
-    // 配列でプレイヤーの役と勝ち負けを返す
 
 function convertToCardRanks(array $cards): array
 {
@@ -37,14 +34,25 @@ function checkHand(int $checkHands1, int $checkHands2)
     // それぞれカードの判定を行う
     if (isStraight($checkHands1, $checkHands2)) {
       $name = STRAIGHT;
+        if (isMinMax($checkHands1, $checkHands2)) {
+            $highCard = min(CARD_RANKS);
+            $lowCard = max(CARD_RANKS);
+            $nama = STRAIGHT;
+        }
     } elseif (isPair($checkHands1, $checkHands1)) {
       $name = PAIR;
     }
+    var_dump($name);
 }
 
 function isStraight($hands1, $hands2): bool
 {
-    return abs($$hands1 - $hands2) === 1;
+    return abs($hands1 - $hands2) === 1;
+}
+
+function isMinMax($hands1, $hands2): bool
+{
+    return abs($hands1 - $hands2) === 1 || (min(CARD_RANKS) - max(CARD_RANKS));
 }
 
 function isPair($hands1, $hands2): bool
@@ -52,11 +60,4 @@ function isPair($hands1, $hands2): bool
     return $hands1 === $hands2;
 }
 
-showDown('CK', 'DJ', 'C10', 'H10');  //=> ['high card', 'pair', 2]
-showDown('CK', 'DJ', 'C3', 'H4');    //=> ['high card', 'straight', 2]
-showDown('C3', 'H4', 'DK', 'SK');    //=> ['straight', 'pair', 1]
-
-
-// 取得したカードのデータ構造を変更した
-// array_chunkでプレイヤー1とプレイヤー2のカードを配列で分割
-// これから役の判定に入っていく？
+showDown('CK', 'DJ', 'C10', 'H8');  //=> ['high card', 'pair', 2]
