52cbcbf6720a7628823c71864025fffd204e5fa6
kensukeohguchi1001 <kensuke.ohguchi@gmail.com>
2022-03-16 07:01:02 +0900

    メモを追記

--- lib/index.php
+++ lib/index.php
@@ -1,29 +1,57 @@
 <?php
 
-// 暗記でもいい、とりあえずツーカードポーカーの復習しよう
-// 50分から
-// 一文字目のアルファベットを削除する
-//    substrとstrlenを使用する、それをarray_mapを使って処理を行う
-//
-// 役を判定するために、カードをランクに変換する
-//    四枚のカード情報が入った配列をarray_mapを使ってランクに変換する
-//    変換したものをarray_chunkで分ける（プレイヤー1のカード, プレイヤー２のカード）
-// 役を判定する、
-//    3つの役をそれぞれ判定する関数を宣言する
-//    ストレートの場合考えないといけないことが一点、Aと2もストレートになるということ
-//    そのパターンのみ別処理を行う
-// 役の判定情報から勝者を判定する、
-//    役の強さを数字で表すconstを定義する
-//    役の強さを判定する、foreachを使って比較する
-// showDownメソッドを呼び出し、指定された内容を戻り値としてかえす
+/*
+ * ◯お題
+ *
+ * 2枚の手札でポーカーを行います。ルールは次の通りです。
+ *
+ * ・プレイヤーは2人です
+ * ・各プレイヤーはトランプ2枚を与えられます
+ * ・ジョーカーはありません
+ * ・与えられたカードから、役を判定します。役は番号が大きくなるほど強くなります
+ *   1. ハイカード：以下の役が一つも成立していない
+ *   2. ペア：2枚のカードが同じ数字
+ *   3. ストレート：2枚のカードが連続している。A は 2 と K の両方と連続しているとみなし、A を含むストレート は、A-2 と K-A の2つです
+ * ・2つの手札について、強さは以下に従います
+ *   1. 2つの手札の役が異なる場合、より上位の役を持つ手札が強いものとする
+ *   2. 2つの手札の役が同じ場合、各カードの数値によって強さを比較する
+ * 　  ・（弱）2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A (強)
+ * 　  ・ハイカード：一番強い数字同士を比較する。左記が同じ数字の場合、もう一枚のカード同士を比較する
+ * 　  ・ペア：ペアの数字を比較する
+ * 　  ・ストレート：一番強い数字を比較する (ただし、A-2 の組み合わせの場合、2 を一番強い数字とする。K-A が最強、A-2 が最弱)
+ * 　  ・数値が同じ場合：引き分け
+ * 　
+ * それぞれの役と勝敗を判定するプログラムを作成ください。
+ *
+ * ◯仕様
+ *
+ * それぞれの役と勝敗を判定するshowDownメソッドを定義してください。
+ * showDownメソッドは引数として、プレイヤー1のカード、プレイヤー1のカード、プレイヤー2のカード、プレイヤー2のカードを取ります。
+ * カードはH1〜H13（ハート）、S1〜S13（スペード）、D1〜D13（ダイヤ）、C1〜C13（クラブ）、となります。ただし、Jは11、Qは12、Kは13、Aは1とします。
+ * showDownメソッドは返り値として、プレイヤー1の役、プレイヤー2の役、勝利したプレイヤーの番号、を返します。引き分けの場合、プレイヤーの番号は0とします。
+ *
+ * ◯実行例
+ *
+ * showDown('CK', 'DJ', 'C10', 'H10')  //=> ['high card', 'pair', 2]
+ * showDown('CK', 'DJ', 'C3', 'H4')  //=> ['high card', 'straight', 2]
+ * showDown('C3', 'H4', 'DK', 'SK')  //=> ['straight', 'pair', 1]
+ * showDown('HJ', 'SK', 'DQ', 'D10')  //=> ['high card', 'high card', 1]
+ * showDown('H9', 'SK', 'DK', 'D10')  //=> ['high card', 'high card', 2]
+ * showDown('H3', 'S5', 'D5', 'D3')  //=> ['high card', 'high card', 0]
+ * showDown('CA', 'DA', 'C2', 'D2')  //=> ['pair', 'pair', 1]
+ * showDown('CK', 'DK', 'CA', 'DA')  //=> ['pair', 'pair', 2]
+ * showDown('C4', 'D4', 'H4', 'S4')  //=> ['pair', 'pair', 0]
+ * showDown('SA', 'DK', 'C2', 'CA')  //=> ['straight', 'straight', 1]
+ * showDown('C2', 'CA', 'S2', 'D3')  //=> ['straight', 'straight', 2]
+ * showDown('S2', 'D3', 'C2', 'H3')  //=> ['straight', 'straight', 0]
+*/
 
+const HIGH_CARD = 'high card';
 const PAIR = 'pair';
 const STRAIGHT = 'straight';
-const HIGH_CARD = 'high_card';
 
-const CARDS = ['1', '2', '3', '4', '5' , '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-
-define("CARD_RANKS",(function () {
+const CARDS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+define('CARD_RANK', (function () {
     $cardRanks = [];
     foreach (CARDS as $index => $card) {
         $cardRanks[$card] = $index;
@@ -31,73 +59,76 @@ define("CARD_RANKS",(function () {
     return $cardRanks;
 })());
 
-const HANDS_RANK = [
+const HAND_RANK = [
     HIGH_CARD => 1,
     PAIR => 2,
-    STRAIGHT => 3
+    STRAIGHT => 3,
 ];
 
-function showDown($card11, $card12, $card21, $card22): array
+function showDown(string $card11, string $card12, string $card21, string $card22): array
 {
-    $substrCardNumber = substrCardNumber([$card11, $card12, $card21, $card22]);
-    $playerCards = array_chunk($substrCardNumber, 2);
-    $hands = array_map(fn ($playerCard) => checkHands($playerCard[0], $playerCard[1]), $playerCards);
-    return [];
+    $cardRanks = convertToCardRanks([$card11, $card12, $card21, $card22]);
+    $playerCardRanks = array_chunk($cardRanks, 2);
+    $hands = array_map(fn ($playerCardRank) => checkHand($playerCardRank[0], $playerCardRank[1]), $playerCardRanks);
+    $winner = decideWinner($hands[0], $hands[1]);
+    return [$hands[0]['name'], $hands[1]['name'], $winner];
 }
 
-function substrCardNumber(array $cards): array
+function convertToCardRanks(array $cards): array
 {
-    return array_map(fn ($card) => CARD_RANKS[substr($card, 1)] , $cards);
+    return array_map(fn ($card) => CARD_RANK[substr($card, 1, strlen($card) - 1)], $cards);
 }
-// 名前の付け方が微妙
 
-function checkHands($card1, $card2): array
+function checkHand(int $cardRank1, int $cardRank2): array
 {
-    $primary = max($card1, $card2);
-    $secondary = min($card1, $card2);
+    $primary = max($cardRank1, $cardRank2);
+    $secondary = min($cardRank1, $cardRank2);
     $name = HIGH_CARD;
 
-    if (isStraight($card1, $card2)) {
+    if (isStraight($cardRank1, $cardRank2)) {
         $name = STRAIGHT;
-        if (isMinMax($card1, $card2)) {
-            $primary = min($card1, $card2);
-            $secondary = max($card1, $card2);
+        if (isMinMax($cardRank1, $cardRank2)) {
+            $primary = min(CARD_RANK);
+            $secondary = max(CARD_RANK);
         }
-    } elseif (isPair($card1, $card2)) {
+    } elseif (isPair($cardRank1, $cardRank2)) {
         $name = PAIR;
     }
 
     return [
         'name' => $name,
-        'rank' => HANDS_RANK[$name],
+        'rank' => HAND_RANK[$name],
         'primary' => $primary,
         'secondary' => $secondary,
     ];
 }
 
-function isStraight($card1, $card2): bool
+function isStraight(int $cardRank1, int $cardRank2): bool
 {
-    return abs($card1 - $card2) === 1 || isMinMax($card1, $card2);
-}
-function isPair($card1, $card2): bool
-{
-    return $card1 === $card2;
+    return abs($cardRank1 - $cardRank2) === 1 || isMinMax($cardRank1, $cardRank2);
 }
 
-function isMinMax($card1, $card2): bool
+function isMinMax(int $cardRank1, int $cardRank2): bool
 {
-    return abs($card1 - $card2) === abs(min(CARD_RANKS) - max(CARD_RANKS));
+    return abs($cardRank1 - $cardRank2) === (max(CARD_RANK) - min(CARD_RANK));
 }
 
-showDown('CK', 'DJ', 'C10', 'H10');  //=> ['high card', 'pair', 2]
-// showDown('CK', 'DJ', 'C3', 'H4');    //=> ['high card', 'straight', 2]
-// showDown('C3', 'H4', 'DK', 'SK');    //=> ['straight', 'pair', 1]
-// showDown('HJ', 'SK', 'DQ', 'D10');   //=> ['high card', 'high card', 1]
-// showDown('H9', 'SK', 'DK', 'D10');   //=> ['high card', 'high card', 2]
-// showDown('H3', 'S5', 'D5', 'D3');    //=> ['high card', 'high card', 0]
-// showDown('CA', 'DA', 'C2', 'D2');    //=> ['pair', 'pair', 1]
-// showDown('CK', 'DK', 'CA', 'DA');    //=> ['pair', 'pair', 2]
-// showDown('C4', 'D4', 'H4', 'S4');    //=> ['pair', 'pair', 0]
-// showDown('SA', 'DK', 'C2', 'CA');    //=> ['straight', 'straight', 1]
-// showDown('C2', 'CA', 'S2', 'D3');    //=> ['straight', 'straight', 2]
-// showDown('S2', 'D3', 'C2', 'H3');    //=> ['straight', 'straight', 0]
+function isPair(int $cardRank1, int $cardRank2): bool
+{
+    return $cardRank1 === $cardRank2;
+}
+
+function decideWinner(array $hand1, array $hand2): int
+{
+    foreach (['rank', 'primary', 'secondary'] as $k) {
+        if ($hand1[$k] > $hand2[$k]) {
+            return 1;
+        }
+
+        if ($hand1[$k] < $hand2[$k]) {
+            return 2;
+        }
+    }
+
+    return 0;
+}
